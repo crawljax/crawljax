e14fa53bc05193926eae8be6468de91c12adc482
Ali Mesbah <amesbah@gmail.com>
2010-01-14 17:57:02 +0000

    rename so that test class is run through Maven automatically.

--- src/test/java/com/crawljax/core/CrawljaxWithPropertiesFileTest.java
+++ src/test/java/com/crawljax/core/CrawljaxWithPropertiesFileTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
-public class CrawljaxControllerWithPropertiesFile {
+public class CrawljaxWithPropertiesFileTest {
 
 	@Test
 	public void runCrawljax() {
