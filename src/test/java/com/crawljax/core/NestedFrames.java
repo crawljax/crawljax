80cb1bf31ace285f6d25e6c95d5827fdef9bfcb2
Ali Mesbah <amesbah@gmail.com>
2010-01-21 18:57:34 +0000

    close the browser.

--- src/test/java/com/crawljax/core/NestedFrames.java
+++ src/test/java/com/crawljax/core/NestedFrames.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.fail;
 
 import java.io.File;
 
+import org.junit.After;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -12,23 +13,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NestedFrames {
+	WebDriver driver = new FirefoxDriver();
 
 	@Test
-	public void testNesterFramesIndex() {
+	public void testNestedFramesIndex() {
 		File index = new File("src/test/site/iframe/page0.html");
-		WebDriver driver = new FirefoxDriver();
 		driver.get("file://" + index.getAbsolutePath());
 
 		driver.switchTo().frame("0.0");
+		System.out.println("DOM: " + driver.getPageSource());
 
 		WebElement button002 = driver.findElement(By.id("button002"));
-
 		try {
 			button002.click();
 		} catch (NoSuchElementException e) {
 			e.printStackTrace();
+
 			fail(e.getMessage());
 		}
+	}
+
+	@After
+	public void close() {
+		driver.close();
 
 	}
 }
