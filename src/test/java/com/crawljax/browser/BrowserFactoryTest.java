73b64eef44b60cd1898ffe90ba9f9ed15af27bf4
Stefan Lenselink <lenselinkstefan@gmail.com>
2010-01-26 14:01:26 +0000

    Added assertions and improved BrowserFactoryTest

--- src/test/java/com/crawljax/browser/BrowserFactoryTest.java
+++ src/test/java/com/crawljax/browser/BrowserFactoryTest.java
@@ -1,6 +1,11 @@
 package com.crawljax.browser;
 
+import org.apache.commons.configuration.ConfigurationException;
 import org.junit.Test;
+
+import com.crawljax.core.configuration.CrawlSpecification;
+import com.crawljax.core.configuration.CrawljaxConfiguration;
+import com.crawljax.util.PropertyHelper;
 
 /**
  * This test, test the (public) operations from the BrowserFactory.
@@ -64,6 +69,29 @@ public class BrowserFactoryTest {
 	@Test(timeout = TIMEOUT)
 	public void testCloseOnlyTwoTimes() {
 		BrowserFactory.close();
+		BrowserFactory.close();
+	}
+
+	/**
+	 * Test opening 4 browsers, 3 requested, 1 returned. close should be done within TIMEOUT.
+	 * 
+	 * @throws ConfigurationException
+	 *             when config is not correct
+	 * @throws InterruptedException
+	 *             when the request for a browser is interupped
+	 */
+	@Test(timeout = TIMEOUT)
+	public void testMultipleBrowsers() throws ConfigurationException, InterruptedException {
+		CrawlSpecification spec = new CrawlSpecification("about:blank");
+		// TODO Stefan. when NuberOfBrowsers specified; use that in stead...
+		spec.setNumberOfThreads(4);
+		CrawljaxConfiguration cfg = new CrawljaxConfiguration();
+		cfg.setCrawlSpecification(spec);
+		PropertyHelper.init(cfg);
+		BrowserFactory.requestBrowser();
+		BrowserFactory.requestBrowser();
+		EmbeddedBrowser b1 = BrowserFactory.requestBrowser();
+		BrowserFactory.freeBrowser(b1);
 		BrowserFactory.close();
 	}
 }
