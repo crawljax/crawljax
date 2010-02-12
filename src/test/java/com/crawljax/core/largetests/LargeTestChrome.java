0f4b0e9859a4ffd30a787985e97515b66496f695
Ali Mesbah <amesbah@gmail.com>
2010-02-12 19:06:49 +0000

    can go faster.

--- src/test/java/com/crawljax/core/largetests/LargeTestChrome.java
+++ src/test/java/com/crawljax/core/largetests/LargeTestChrome.java
@@ -12,8 +12,8 @@ import com.crawljax.core.configuration.CrawljaxConfiguration;
 
 public class LargeTestChrome extends LargeTestSuper {
 
-	private static final int waitAfterEvent = 800;
-	private static final int waitAfterReload = 400;
+	private static final int waitAfterEvent = 100;
+	private static final int waitAfterReload = 100;
 	private static BrowserType browser = BrowserType.chrome;
 	private static final String INDEX = "http://spci.st.ewi.tudelft.nl/demo/testsite/";
 
