61ca40c9fe3fab98e1319fa591fcdc9f840763ab
Ali Mesbah <amesbah@gmail.com>
2010-02-12 19:49:18 +0000

    check on null

--- src/main/java/com/crawljax/util/XPathHelper.java
+++ src/main/java/com/crawljax/util/XPathHelper.java
@@ -347,15 +347,18 @@ public final class XPathHelper {
 	 *         text()
 	 */
 	public static String stripXPathToElement(String xpath) {
-		if (xpath.toLowerCase().indexOf("/text()") != -1) {
-			xpath = xpath.substring(0, xpath.toLowerCase().indexOf("/text()"));
+		if (xpath != null && !xpath.equals("")) {
+			if (xpath.toLowerCase().indexOf("/text()") != -1) {
+				xpath = xpath.substring(0, xpath.toLowerCase().indexOf("/text()"));
+			}
+			if (xpath.toLowerCase().indexOf("/comment()") != -1) {
+				xpath = xpath.substring(0, xpath.toLowerCase().indexOf("/comment()"));
+			}
+			if (xpath.indexOf("@") != -1) {
+				xpath = xpath.substring(0, xpath.indexOf("@") - 1);
+			}
 		}
-		if (xpath.toLowerCase().indexOf("/comment()") != -1) {
-			xpath = xpath.substring(0, xpath.toLowerCase().indexOf("/comment()"));
-		}
-		if (xpath.indexOf("@") != -1) {
-			xpath = xpath.substring(0, xpath.indexOf("@") - 1);
-		}
+
 		return xpath;
 	}
 
