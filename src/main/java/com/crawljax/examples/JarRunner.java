d9be9db072eba592c1f1d9955f9512cb451065cc
Frank Groeneveld <frankgroeneveld@gmail.com>
2010-04-12 14:34:31 +0000

    Add a runner to be used as main after packaging.

---
+++ src/main/java/com/crawljax/examples/JarRunner.java
@@ -0,0 +1,27 @@
+package com.crawljax.examples;
+
+import com.crawljax.core.CrawljaxController;
+import com.crawljax.core.configuration.CrawlSpecification;
+import com.crawljax.core.configuration.CrawljaxConfiguration;
+
+public class JarRunner {
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.err.println("Please give an url as first argument to Crawljax");
+			System.exit(1);
+		}
+		CrawlSpecification crawler = new CrawlSpecification(args[0]);
+		crawler.clickDefaultElements();
+
+		CrawljaxConfiguration config = new CrawljaxConfiguration();
+
+		config.setCrawlSpecification(crawler);
+
+		try {
+			CrawljaxController crawljax = new CrawljaxController(config);
+			crawljax.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
