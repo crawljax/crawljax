721f46f35d3b875f91e2ba3f896042ee3f6e3b34
Ali Mesbah <amesbah@gmail.com>
2010-04-16 09:55:46 +0000

    ignore the test (maven)

---
+++ src/test/java/com/crawljax/util/PrettyHTMLTestIgnore.java
@@ -0,0 +1,28 @@
+package com.crawljax.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+
+import org.junit.Ignore;
+
+public class PrettyHTMLTestIgnore {
+
+	private static final String TESTFILE = "src/test/java/com/crawljax/util/tuduDombefore.html";
+	private static final String CONTROLFILE =
+	        "src/test/java/com/crawljax/util/tuduDombefore.html.tidy";
+
+	@Ignore
+	public void prettifyHTML() {
+		String testdom = Helper.getContent(new File(TESTFILE));
+		String controldom = Helper.getContent(new File(CONTROLFILE));
+
+		assertNotNull("File should be read", testdom);
+		assertNotNull("File should be read", controldom);
+
+		testdom = PrettyHTML.prettyHTML(testdom);
+
+		assertEquals(controldom, testdom);
+	}
+}
