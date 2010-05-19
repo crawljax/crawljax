0d668be51c0cc59319e2423ac6c6b23192680027
Frank Groeneveld <frankgroeneveld@gmail.com>
2010-05-19 08:29:50 +0000

    Remove unused imports

--- src/main/java/com/crawljax/core/configuration/CrawljaxConfigurationReader.java
+++ src/main/java/com/crawljax/core/configuration/CrawljaxConfigurationReader.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import com.crawljax.browser.BrowserBuilder;
 import com.crawljax.browser.EmbeddedBrowser.BrowserType;
