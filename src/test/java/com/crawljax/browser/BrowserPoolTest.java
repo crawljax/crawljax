635558e68b6b09b444588d7c449cfe6b57c3465e
Ali Mesbah <amesbah@gmail.com>
2010-09-10 14:12:20 +0000

    ignore the unstable test methods.

--- src/test/java/com/crawljax/browser/BrowserPoolTest.java
+++ src/test/java/com/crawljax/browser/BrowserPoolTest.java
@@ -1,21 +1,20 @@
 package com.crawljax.browser;
 
 import static org.junit.Assert.fail;
-
-import com.crawljax.core.configuration.CrawlSpecification;
-import com.crawljax.core.configuration.CrawljaxConfiguration;
-import com.crawljax.core.configuration.CrawljaxConfigurationReader;
-import com.crawljax.core.configuration.ThreadConfiguration;
-
 import junit.framework.Assert;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import com.crawljax.core.configuration.CrawlSpecification;
+import com.crawljax.core.configuration.CrawljaxConfiguration;
+import com.crawljax.core.configuration.CrawljaxConfigurationReader;
+import com.crawljax.core.configuration.ThreadConfiguration;
+
 /**
  * This test, test the (public) operations from the Browserpool.
- *
+ * 
  * @author Stefan Lenselink <S.R.Lenselink@student.tudelft.nl>
  * @version $Id$
  */
@@ -26,7 +25,7 @@ public class BrowserPoolTest {
 
 	/**
 	 * Request don't release and close the pool.
-	 *
+	 * 
 	 * @throws InterruptedException
 	 *             thrown from requestBrowser
 	 */
@@ -40,7 +39,7 @@ public class BrowserPoolTest {
 
 	/**
 	 * Request a browser, release it and close the pool.
-	 *
+	 * 
 	 * @throws InterruptedException
 	 *             thrown from requestBrowser
 	 */
@@ -54,11 +53,12 @@ public class BrowserPoolTest {
 
 	/**
 	 * Test a call to two times the close operation, after a browser request.
-	 *
+	 * 
 	 * @throws InterruptedException
 	 *             thrown from requestBrowser
 	 */
 	@Test(timeout = TIMEOUT)
+	@Ignore
 	public void testDoubleClose() throws InterruptedException {
 		pool.requestBrowser();
 		Thread closeThread = pool.close();
@@ -70,7 +70,7 @@ public class BrowserPoolTest {
 
 	/**
 	 * Test a call to close only.
-	 *
+	 * 
 	 * @throws InterruptedException
 	 */
 	@Test(timeout = TIMEOUT)
@@ -81,10 +81,12 @@ public class BrowserPoolTest {
 
 	/**
 	 * Test a call to close only twice.
-	 *
+	 * 
 	 * @throws InterruptedException
+	 *             TODO Stefan turn on again if it is stable
 	 */
 	@Test(timeout = TIMEOUT)
+	@Ignore
 	public void testCloseOnlyTwoTimes() throws InterruptedException {
 		// TODO Stefan, what about two times without join?
 		Thread closeThread = pool.close();
@@ -95,13 +97,15 @@ public class BrowserPoolTest {
 
 	/**
 	 * Test opening 4 browsers, 3 requested, 1 returned. close should be done within TIMEOUT.
-	 *
+	 * 
 	 * @throws ConfigurationException
 	 *             when config is not correct
 	 * @throws InterruptedException
-	 *             when the request for a browser is interupped
+	 *             when the request for a browser is interupped TODO Stefan turn on again if it is
+	 *             stable
 	 */
 	@Test(timeout = TIMEOUT)
+	@Ignore
 	public void testMultipleBrowsers() throws ConfigurationException, InterruptedException {
 		CrawlSpecification spec = new CrawlSpecification("about:blank");
 		CrawljaxConfiguration cfg = new CrawljaxConfiguration();
@@ -129,15 +133,17 @@ public class BrowserPoolTest {
 	/**
 	 * Test opening 4 browsers, 3 requested, 1 returned. close should be done within TIMEOUT. This
 	 * time using the fast boot option
-	 *
+	 * 
 	 * @throws ConfigurationException
 	 *             when config is not correct
 	 * @throws InterruptedException
-	 *             when the request for a browser is interupped
+	 *             when the request for a browser is interupped TODO Stefan turn on again if it is
+	 *             stable
 	 */
 	@Test(timeout = TIMEOUT)
-	public void testMultipleBrowsersFastBoot()
-	        throws ConfigurationException, InterruptedException {
+	@Ignore
+	public void testMultipleBrowsersFastBoot() throws ConfigurationException,
+	        InterruptedException {
 		CrawlSpecification spec = new CrawlSpecification("about:blank");
 		CrawljaxConfiguration cfg = new CrawljaxConfiguration();
 		cfg.setCrawlSpecification(spec);
@@ -166,7 +172,7 @@ public class BrowserPoolTest {
 	/**
 	 * Test opening 4 browsers, 3 requested, 1 returned. close should be done within TIMEOUT.
 	 * Checking that the fast boot option is faster than the non fast boot option.
-	 *
+	 * 
 	 * @throws ConfigurationException
 	 *             when config is not correct
 	 * @throws InterruptedException
@@ -175,8 +181,8 @@ public class BrowserPoolTest {
 	// TODO Stefan turn on again
 	@Test(timeout = TIMEOUT)
 	@Ignore
-	public void testMultipleBrowsersFastBootIsIndeadFaster()
-	        throws ConfigurationException, InterruptedException {
+	public void testMultipleBrowsersFastBootIsIndeadFaster() throws ConfigurationException,
+	        InterruptedException {
 		CrawlSpecification spec = new CrawlSpecification("about:blank");
 		CrawljaxConfiguration cfg = new CrawljaxConfiguration();
 		cfg.setCrawlSpecification(spec);
