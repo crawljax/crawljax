485e5fb1d85271195c49b7908346cde633f9061f
Ali Mesbah <amesbah@gmail.com>
2010-11-05 15:25:57 +0000

    The large test with the HtmlUnit driver. It is very fast and it passes...

---
+++ src/test/java/com/crawljax/core/largetests/LargeHtmlUnitTest.java
@@ -0,0 +1,56 @@
+package com.crawljax.core.largetests;
+
+import static org.junit.Assert.fail;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.junit.BeforeClass;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.crawljax.browser.EmbeddedBrowser;
+import com.crawljax.browser.EmbeddedBrowserBuilder;
+import com.crawljax.browser.WebDriverBackedEmbeddedBrowser;
+import com.crawljax.core.CrawljaxController;
+import com.crawljax.core.CrawljaxException;
+import com.crawljax.core.configuration.CrawljaxConfiguration;
+import com.crawljax.core.configuration.CrawljaxConfigurationReader;
+
+public class LargeHtmlUnitTest extends LargeTestSuper {
+
+	private static final int waitAfterEvent = 100;
+	private static final int waitAfterReload = 100;
+	private static final String INDEX = "http://spci.st.ewi.tudelft.nl/demo/testsite/";
+
+	/**
+	 * Runs crawljax.
+	 * 
+	 * @throws java.lang.Exception
+	 *             when error while crawling
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() {
+
+		CrawljaxConfiguration crawljaxConfiguration = new CrawljaxConfiguration();
+		crawljaxConfiguration.setCrawlSpecification(getCrawlSpecification(INDEX, waitAfterEvent,
+		        waitAfterReload));
+		addPlugins(crawljaxConfiguration);
+
+		crawljaxConfiguration.setBrowserBuilder(new EmbeddedBrowserBuilder() {
+			@Override
+			public EmbeddedBrowser buildEmbeddedBrowser(CrawljaxConfigurationReader configuration) {
+				return WebDriverBackedEmbeddedBrowser.withDriver(new HtmlUnitDriver(true));
+			}
+		});
+
+		try {
+			new CrawljaxController(crawljaxConfiguration).run();
+		} catch (ConfigurationException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		} catch (CrawljaxException e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+
+	}
+
+}
