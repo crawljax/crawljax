0aae495eaf25ac51feac0c923975991f71b4ff0c
Ali Mesbah <amesbah@gmail.com>
2010-11-05 16:06:19 +0000

    rename

--- src/test/java/com/crawljax/core/largetests/LargeTestHtmlUnit.java
+++ src/test/java/com/crawljax/core/largetests/LargeTestHtmlUnit.java
@@ -14,7 +14,7 @@ import com.crawljax.core.CrawljaxException;
 import com.crawljax.core.configuration.CrawljaxConfiguration;
 import com.crawljax.core.configuration.CrawljaxConfigurationReader;
 
-public class LargeHtmlUnitTest extends LargeTestSuper {
+public class LargeTestHtmlUnit extends LargeTestSuper {
 
 	private static final int waitAfterEvent = 100;
 	private static final int waitAfterReload = 100;
