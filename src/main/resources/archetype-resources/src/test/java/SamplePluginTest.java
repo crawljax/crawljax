7c99ace948ed1cbdb1f28bc1692408a57b164c00
Ali Mesbah <amesbah@gmail.com>
2012-12-25 20:48:50 -0800

    Initial import

---
+++ src/main/resources/archetype-resources/src/test/java/SamplePluginTest.java
@@ -0,0 +1,14 @@
+package sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SamplePluginTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
