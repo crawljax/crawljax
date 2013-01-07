34ac31e0fa13e68851e7dcf786196dd828d45d92
Alex Nederlof <alex@nederlof.com>
2013-01-07 18:38:08 +0100

    Double click now directs you to the state

--- src/main/resources/skeleton/js/graphbuilder.js
+++ src/main/resources/skeleton/js/graphbuilder.js
@@ -22,21 +22,21 @@ renderer.run();
 
 graphics.node(function(node) {
 	// This time it's a group of elements: http://www.w3.org/TR/SVG/struct.html#Groups
-	var ui = Viva.Graph.svg('g'),
+	var ui = Viva.Graph.svg('g');
 	// Create SVG text element with user id as content
-	svgText = Viva.Graph.svg('text').attr('y', '-4px').text(node.id),
-	img = Viva.Graph.svg('image')
-	.attr('width', nodeSize)
-	.attr('height', nodeSize)
-	.link(node.data.url); 
-	$(img).dblclick(function() {console.log("click!");});
+	var svgText = Viva.Graph.svg('text').attr('y', '-4px').text(node.id);
+	var img = Viva.Graph.svg('image')
+		.attr('width', nodeSize)
+		.attr('height', nodeSize)
+		.link(node.data.img); 
+	$(img).dblclick(function() { window.location = node.data.url;});
 	ui.append(svgText);
 	ui.append(img);	
 	stroke = Viva.Graph.svg('rect')
-	.attr("style", "fill:none;stroke-width:1;stroke:black;")
-	.attr('width', nodeSize+1)
-	.attr('height', nodeSize+1)
-	ui.append(stroke);
+		.attr("style", "fill:none;stroke-width:1;stroke:black;")
+		.attr('width', nodeSize+1)
+		.attr('height', nodeSize+1)
+		ui.append(stroke);
 	return ui;
 }).placeNode(function(nodeUI, pos) {
 	nodeUI.attr('transform', 
