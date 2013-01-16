486c99f164163795353bc52a343fd21714d91389
Ali Mesbah <amesbah@gmail.com>
2013-01-15 22:09:22 -0800

    chromedriver executable commented out for now

--- core/src/test/java/com/crawljax/core/largetests/LargeTestChrome.java
+++ core/src/test/java/com/crawljax/core/largetests/LargeTestChrome.java
@@ -25,9 +25,8 @@ public class LargeTestChrome extends LargeTestSuper {
 	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		// TODO
-		System.setProperty("webdriver.chrome.driver",
-		        "//Users//amesbah//repos//git//crawljax//chromedriver 4");
+		// System.setProperty("webdriver.chrome.driver",
+		// "//Users//amesbah//repos//git//crawljax//chromedriver 4");
 		CrawljaxConfiguration crawljaxConfiguration = new CrawljaxConfiguration();
 		crawljaxConfiguration.setCrawlSpecification(getCrawlSpecification(INDEX, waitAfterEvent,
 		        waitAfterReload));
