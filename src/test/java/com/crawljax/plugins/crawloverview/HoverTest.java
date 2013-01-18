590734e78deeaa1cb6cc3a9c29c2942a5624ac7a
Alex Nederlof <alex@nederlof.com>
2013-01-18 13:34:27 +0100

    Fixing the hover test

--- src/test/java/com/crawljax/plugins/crawloverview/HoverTest.java
+++ src/test/java/com/crawljax/plugins/crawloverview/HoverTest.java
@@ -7,17 +7,24 @@ import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import org.eclipse.jetty.util.resource.Resource;
 import org.hamcrest.CustomMatcher;
 import org.hamcrest.Factory;
+import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.crawljax.crawltests.BaseCrawler;
 import com.crawljax.plugins.crawloverview.model.CandidateElementPosition;
@@ -56,7 +63,15 @@ public class HoverTest {
 		return new ElementMatcher(new CandidateElementPosition(null, point, size));
 	}
 
+	private static final int MIN_HEIGHT = 500;
+
+	private static final int MIN_WIDHT = 1024;
+
+	private static final Logger LOG = LoggerFactory.getLogger(HoverTest.class);
+
 	private static OutPutModel result;
+
+	private static boolean resolutionBigEnough;
 
 	@ClassRule
 	public static final TempDirInTargetFolder tmpDirRul = new TempDirInTargetFolder(
@@ -70,6 +85,17 @@ public class HoverTest {
 		CrawlOverview plugin = new CrawlOverview(outFile);
 		hoverSiteCrawl.crawlWith(plugin);
 		result = plugin.getResult();
+		resolutionBigEnough = calculateResolution(outFile);
+	}
+
+	private static boolean calculateResolution(File outFile) throws IOException {
+		File indexScreensShot = new File(outFile, "screenshots/index.png");
+		assertThat(indexScreensShot.exists(), is(true));
+		BufferedImage img = ImageIO.read(indexScreensShot);
+		boolean enough = img.getWidth() > MIN_WIDHT && img.getHeight() > MIN_HEIGHT;
+		LOG.debug("Images size is {} x {}. Good enough = {}",
+		        img.getWidth(), img.getHeight(), enough);
+		return enough;
 	}
 
 	@Test
@@ -78,6 +104,7 @@ public class HoverTest {
 		assertThat(state, is(notNullValue()));
 		List<CandidateElementPosition> candidates = state.getCandidateElements();
 		assertThat("Number of hovers", candidates, hasSize(3));
+		Assume.assumeTrue(resolutionBigEnough);
 		assertThat(candidates, hasItem(element(new Point(48, 118), new Dimension(52, 16))));
 		assertThat(candidates, hasItem(element(new Point(48, 137), new Dimension(51, 16))));
 		assertThat(candidates, hasItem(element(new Point(48, 156), new Dimension(200, 16))));
@@ -89,6 +116,7 @@ public class HoverTest {
 		assertThat(state, is(notNullValue()));
 		List<CandidateElementPosition> candidates = state.getCandidateElements();
 		assertThat(candidates, hasSize(1));
+		Assume.assumeTrue(resolutionBigEnough);
 		assertThat(candidates, hasItem(element(new Point(58, 147), new Dimension(89, 16))));
 	}
 
@@ -98,6 +126,7 @@ public class HoverTest {
 		assertThat(state, is(notNullValue()));
 		List<CandidateElementPosition> candidates = state.getCandidateElements();
 		assertThat(candidates, hasSize(1));
+		Assume.assumeTrue(resolutionBigEnough);
 		assertThat(candidates, hasItem(element(new Point(60, 168), new Dimension(51, 16))));
 	}
 
