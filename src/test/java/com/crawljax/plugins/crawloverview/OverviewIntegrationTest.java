1eafef459e622662e2faa11133fc04971d46722d
Alex Nederlof <alex@nederlof.com>
2013-01-19 17:47:58 +0100

    setup the integration test

--- src/test/java/com/crawljax/plugins/crawloverview/OverviewIntegrationTest.java
+++ src/test/java/com/crawljax/plugins/crawloverview/OverviewIntegrationTest.java
@@ -1,16 +1,72 @@
 package com.crawljax.plugins.crawloverview;
 
-import org.junit.Assert;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jetty.server.AbstractConnector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverBackedSelenium;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.thoughtworks.selenium.DefaultSelenium;
 
 public class OverviewIntegrationTest {
 
 	@ClassRule
 	public static final RunHoverCrawl HOVER_CRAWL = new RunHoverCrawl();
 
+	private static final Logger LOG = LoggerFactory.getLogger(OverviewIntegrationTest.class);
+
+	private static Server server;
+	private static DefaultSelenium selenium;
+
+	private static WebDriver driver;
+
+	@BeforeClass
+	public static void setup() throws Exception {
+		LOG.debug("Starting Jetty");
+		server = new Server(0);
+		String url = setupJetty();
+		LOG.info("Jetty started on {}", url);
+		driver = new FirefoxDriver();
+		LOG.debug("Starting selenium");
+		selenium = new WebDriverBackedSelenium(driver, url);
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	private static String setupJetty() throws Exception {
+		WebAppContext webAppContext = new WebAppContext();
+		webAppContext.setWar(RunHoverCrawl.getOutDir().getAbsolutePath());
+		server.setHandler(webAppContext);
+		server.start();
+		int port = ((AbstractConnector) server.getConnectors()[0]).getLocalPort();
+		String url = "http://localhost:" + port;
+		return url;
+	}
+
 	@Test
 	public void test() {
-		Assert.assertTrue(true);
+		selenium.open("/");
+		WebElement brand = driver.findElement(By.cssSelector("a.brand"));
+		assertThat(brand.getText(), is("Crawl overview"));
+	}
+
+	@AfterClass
+	public static void tearDown() throws Exception {
+		server.stop();
+		selenium.stop();
 	}
 }
