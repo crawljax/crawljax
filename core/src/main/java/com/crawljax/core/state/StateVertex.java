fda8df6de17b9e636db603e3db5b3fd9dc321e2f
Alex Nederlof <alex@nederlof.com>
2013-01-30 16:36:17 +0100

    Dom size now reported in KB (issue #116)

--- core/src/main/java/com/crawljax/core/state/StateVertex.java
+++ core/src/main/java/com/crawljax/core/state/StateVertex.java
@@ -15,7 +15,6 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import com.crawljax.core.CandidateCrawlAction;
 import com.crawljax.core.CandidateElement;
