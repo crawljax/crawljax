cca1eace4151e2f2c01cd00a7eaec91cd7c624a7
Alex Nederlof <alex@nederlof.com>
2013-02-28 11:08:15 -0800

    Improved proxy toString and harderend the toBeanMap method

--- plugins/crawloverview-plugin/src/main/java/com/crawljax/plugins/crawloverview/BeanToReadableMap.java
+++ plugins/crawloverview-plugin/src/main/java/com/crawljax/plugins/crawloverview/BeanToReadableMap.java
@@ -7,6 +7,8 @@ import java.util.Collection;
 import java.util.Map.Entry;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
@@ -24,6 +26,8 @@ class BeanToReadableMap {
 	private static final String CAMEL_REGEX = String.format("%s|%s|%s",
 	        "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])", "(?<=[A-Za-z])(?=[^A-Za-z])");
 
+	private static final Logger LOG = LoggerFactory.getLogger(BeanToReadableMap.class);
+
 	public static ImmutableMap<String, String> toMap(Object o) {
 		Builder<String, String> builder = ImmutableMap.builder();
 		for (Method method : o.getClass().getMethods()) {
@@ -34,6 +38,11 @@ class BeanToReadableMap {
 		return builder.build();
 	}
 
+	private static boolean isGetter(Method method) {
+		return method.getName().startsWith("get") && method.getParameterTypes().length == 0
+		        && !"getClass".equals(method.getName());
+	}
+
 	private static Entry<String, String> addMethodToMap(Object o, Method method) {
 		try {
 			Object[] noArgs = null;
@@ -41,13 +50,15 @@ class BeanToReadableMap {
 			return ImmutablePair.of(asName(method), toString(result));
 
 		} catch (Exception e) {
-			throw new CrawlOverviewException("Could not parse bean " + o.toString() + " because "
-			        + e.getMessage(), e);
+			LOG.error("Could not parse bean {} because {}", o, e.getMessage(), e);
+			return ImmutablePair.of(asName(method), "Unreadable entry");
 		}
 	}
 
 	private static String toString(Object result) {
-		if (result instanceof Collection<?>) {
+		if (result == null) {
+			return "null";
+		} else if (result instanceof Collection<?>) {
 			return asHtmlList((Collection<?>) result);
 		} else {
 			return escapeHtml(result.toString());
@@ -68,11 +79,6 @@ class BeanToReadableMap {
 			sb.append("<li>").append(escapeHtml(object.toString())).append("</li>");
 		}
 		return sb.append("</ul>").toString();
-	}
-
-	private static boolean isGetter(Method method) {
-		return method.getName().startsWith("get") && method.getParameterTypes().length == 0
-		        && !"getClass".equals(method.getName());
 	}
 
 	private static String splitCamelCase(String s) {
