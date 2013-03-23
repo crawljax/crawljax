23d3873715ade3870fb2e840f3c6b75f04adce6a
Gord Larson <gordlarson@gmail.com>
2013-03-23 14:02:39 -0700

    Sample site test work

--- core/src/test/java/com/crawljax/test/WebServerTest.java
+++ core/src/test/java/com/crawljax/test/WebServerTest.java
@@ -6,11 +6,16 @@ import org.eclipse.jetty.util.resource.Resource;
 import java.io.IOException;
 import com.crawljax.core.CrawljaxException;
 
+
 import org.junit.Test;
 
 public class WebServerTest {
 	private URL site;
 	private WebServer server;
+	private String siteString;
+	
+	static final int MAX_PORT = 65535;
+	static final int MIN_PORT = 0;
 	
 	private void setup() throws Exception {
 		site = BaseCrawler.class.getResource("/site");
@@ -25,13 +30,16 @@ public class WebServerTest {
 	@Test
 	public void testSiteUrl() throws Exception {
 		setup();
-		assertEquals(server.getSiteUrl(), site);
+		site = new URL("http", "localhost", server.getPort(), "/");
+		assertEquals(site.getPort(), server.getSiteUrl().getPort());
+		assertTrue(site.getPath().equals(server.getSiteUrl().getPath()));
 	}
 	
 	@Test
 	public void testPort() throws Exception {
 		setup();
-		assertEquals(server.getPort(), 0);
+		assertTrue(server.getPort() >= MIN_PORT);
+		assertTrue(server.getPort() <= MAX_PORT);
 	}
 
 }
--- test-utils/src/test/java/com/crawljax/crawljax_plugins_plugin/SampleCrawlersTest.java
+++ test-utils/src/test/java/com/crawljax/crawljax_plugins_plugin/SampleCrawlersTest.java
@@ -2,7 +2,8 @@ package com.crawljax.crawljax_plugins_plugin;
 
 import static com.crawljax.browser.matchers.StateFlowGraphMatchers.hasEdges;
 import static com.crawljax.browser.matchers.StateFlowGraphMatchers.hasStates;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+import java.net.URL;
 
 import org.junit.Test;
 
@@ -12,39 +13,116 @@ import com.crawljax.crawltests.SimpleInputSiteCrawl;
 import com.crawljax.crawltests.SimpleJsSiteCrawl;
 import com.crawljax.crawltests.SimpleSiteCrawl;
 import com.crawljax.crawltests.SimpleXpathCrawl;
+import com.crawljax.test.BaseCrawler;
+import com.crawljax.test.WebServer;
 
 public class SampleCrawlersTest {
-
-	@Test
-	public void testSimpleCrawler() throws Exception {
-		CrawlSession crawl = new SimpleSiteCrawl().crawl();
-		StateFlowGraph stateFlowGraph = crawl.getStateFlowGraph();
-		assertThat(stateFlowGraph, hasStates(SimpleSiteCrawl.NUMBER_OF_STATES));
-		assertThat(stateFlowGraph, hasEdges(SimpleSiteCrawl.NUMBER_OF_EDGES));
+	private BaseCrawler crawler;
+	private CrawlSession crawl;
+	private int numStates;
+	private int numEdges;
+	
+	private void setupSimpleCrawler() throws Exception {
+		crawler = new SimpleSiteCrawl();
+		crawl = crawler.crawl();
+		numStates = SimpleSiteCrawl.NUMBER_OF_STATES;
+		numEdges = SimpleSiteCrawl.NUMBER_OF_EDGES;
+	}
+	
+	private void setupJsCrawler() throws Exception {
+		crawler = new SimpleJsSiteCrawl();
+		crawl = crawler.crawl();
+		numStates = SimpleJsSiteCrawl.NUMBER_OF_STATES;
+		numEdges = SimpleJsSiteCrawl.NUMBER_OF_EDGES;
+	}
+	
+	private void setupInputCrawler() throws Exception {
+		crawler = new SimpleInputSiteCrawl();
+		crawl = crawler.crawl();
+		numStates = SimpleInputSiteCrawl.NUMBER_OF_STATES;
+		numEdges = SimpleInputSiteCrawl.NUMBER_OF_EDGES;
+	}
+	
+	private void setupXpathCrawl() throws Exception {
+		crawler = new SimpleXpathCrawl();
+		crawl = crawler.crawl();
+		numStates = SimpleInputSiteCrawl.NUMBER_OF_STATES;
+		numEdges = SimpleInputSiteCrawl.NUMBER_OF_EDGES;
 	}
 
 	@Test
-	public void testJSCrawler() throws Exception {
-		CrawlSession crawl = new SimpleJsSiteCrawl().crawl();
-		StateFlowGraph stateFlowGraph = crawl.getStateFlowGraph();
-		assertThat(stateFlowGraph, hasStates(SimpleJsSiteCrawl.NUMBER_OF_STATES));
-		assertThat(stateFlowGraph, hasEdges(SimpleJsSiteCrawl.NUMBER_OF_EDGES));
+	public void testSimpleCrawlerFlowGraph() throws Exception {
+		setupSimpleCrawler();
+		stateFlowGraphTestHelper();
+	}
+	
+	@Test
+	public void testSimpleCrawlerUrl() throws Exception {
+		setupSimpleCrawler();
+		urlTestHelper();
+	}
+	
+	@Test(expected=AssertionError.class)
+	public void testSimpleCrawlerShowWebSite() throws Exception {
+		setupSimpleCrawler();
+		//showWebSiteTestHelper();
+		crawler.showWebSite();
+	}
+
+	@Test
+	public void testJsCrawlerFlowGraph() throws Exception {
+		setupJsCrawler();
+		stateFlowGraphTestHelper();
+	}
+	
+	@Test
+	public void testJsCrawlerUrl() throws Exception{
+		setupJsCrawler();
+		urlTestHelper();
+	}
+	
+	@Test(expected=AssertionError.class)
+	public void testJsCrawlerShowWebSite() throws Exception {
+		setupJsCrawler();
+		//showWebSiteTestHelper();
 	}
 
 	@Test
 	public void testInputCrawler() throws Exception {
-		CrawlSession crawl = new SimpleInputSiteCrawl().crawl();
-		StateFlowGraph stateFlowGraph = crawl.getStateFlowGraph();
-		assertThat(stateFlowGraph, hasStates(SimpleInputSiteCrawl.NUMBER_OF_STATES));
-		assertThat(stateFlowGraph, hasEdges(SimpleInputSiteCrawl.NUMBER_OF_EDGES));
+		setupInputCrawler();
+		stateFlowGraphTestHelper();
+		urlTestHelper();
+		//showWebSiteTestHelper();
 	}
 
 	@Test
 	public void testSimpleXPathCrawl() throws Exception {
-		// new SimpleXpathCrawl().showWebSite();
-		CrawlSession crawl = new SimpleXpathCrawl().crawl();
-		StateFlowGraph stateFlowGraph = crawl.getStateFlowGraph();
-		assertThat(stateFlowGraph, hasStates(SimpleInputSiteCrawl.NUMBER_OF_STATES));
-		assertThat(stateFlowGraph, hasEdges(SimpleInputSiteCrawl.NUMBER_OF_EDGES));
+		setupXpathCrawl();
+		stateFlowGraphTestHelper();
+		urlTestHelper();
+		//showWebSiteTestHelper();
 	}
+	
+	private void stateFlowGraphTestHelper() throws Exception {
+		StateFlowGraph stateFlowGraph = crawl.getStateFlowGraph();
+		assertThat(stateFlowGraph, hasStates(numStates));
+		assertThat(stateFlowGraph, hasEdges(numEdges));
+	}
+	
+	
+	private void urlTestHelper() throws Exception {
+		WebServer server = crawler.getWebServer();
+		URL site = new URL("http", "localhost", server.getPort(), "/");
+		assertTrue(site.getPath().equals(server.getSiteUrl().getPath()));
+	}
+	
+	public void showWebSiteTestHelper() throws AssertionError, Exception {
+		try {
+			crawler.showWebSite();
+		}
+		catch (Exception e) {
+			
+		}
+	}
+
 }
