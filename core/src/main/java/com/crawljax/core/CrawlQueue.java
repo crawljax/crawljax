e7157845d3d400f8fa70401bb670b71778422301
Jae-Hwan Jung <midlife83@gmail.com>
2013-03-24 21:02:01 -0700

    Improvement to ISSUE #102. Revamped CrawlQueue.java to make it completely thread-safe in addition to many details mentioned in the comments in the file. CrawlerExecuter.java is modified to make the thread pool size fixed and use the bounded version of CrawlQueue. CrawlerExecutorTest is modified to address ISSUE# 75 and to test further for the revamped CrawlQueue and CrawlerExecutor.

--- core/src/main/java/com/crawljax/core/CrawlQueue.java
+++ core/src/main/java/com/crawljax/core/CrawlQueue.java
@@ -4,97 +4,461 @@
 package com.crawljax.core;
 
 import java.util.Collection;
-import java.util.Stack;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
-
-import net.jcip.annotations.GuardedBy;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * This class implements a BlockingQueue with Runnable as its Generic type and extends Stack with
+ * Original: This class implements a BlockingQueue with Runnable as its Generic type and extends Stack with
  * also Runnable as generic type. This class is used in the ThreadPoolExecutor and its used to store
- * separate threads in a Queue like fashion (FILO).
+ * separate threads in a Queue like fashion (FILO). </br>
  * 
- * @author Stefan Lenselink <S.R.Lenselink@student.tudelft.nl>
+ * <br> Major Overhaul: This class is now implements a BlockingQueue with a LinkedList as its data abstraction rep
+ * instead of extending Stack. This is done to make it easy to change implementation between FIFO and FILO.
+ * Since there's no random access involved, speed is the same as using Stack. This class is used in the ThreadPoolExecutor 
+ * and processes elements in FILO (first-in-last-out). This is now also thread-safe. 
+ * Lastly, this class can be either bounded or unbounded.
+ * 
+ * @author Stefan Lenselink <S.R.Lenselink@student.tudelft.nl> (original implementation)</br>
+ * @author Jae-Hwan Jung <jaehwan.jeff.jung@gmail.com> (Major Overhaul on March 23, 2013)
+ * @since March 23, 2013  
  */
-public class CrawlQueue extends Stack<Runnable> implements BlockingQueue<Runnable> {
-
+public class CrawlQueue implements BlockingQueue<Runnable> {
+	
 	/**
-	 * Auto generated serialVersionUID.
+	 * Data Abstraction Rep
 	 */
-	private static final long serialVersionUID = 4656244727801517204L;
-
-	@Override
-	public int drainTo(Collection<? super Runnable> c) {
-		return drainTo(c, Integer.MAX_VALUE);
+	LinkedList<Runnable> runnables; 
+	
+	/**
+	 * Capacity of this BlockingQueue.	
+	 */
+	private final int capacity;
+	
+	/**
+	 * Number of items in the Queue
+	 */
+	private AtomicInteger count = new AtomicInteger();
+	
+	/**
+	 * Simple reentrant lock
+	 */
+	private final ReentrantLock lock = new ReentrantLock();
+	
+	/**
+	 * Condition for put()
+	 */
+	private final Condition isNotFull = lock.newCondition() ;
+	
+	/**
+	 * Condition for take()
+	 */
+	private final Condition isNotEmpty = lock.newCondition();
+	
+	/**
+	 * Default constructor making this BlockingQueue unbounded
+	 */
+	public CrawlQueue() {
+		super();
+		this.capacity = Integer.MAX_VALUE;
+		runnables = new LinkedList<Runnable>();
+	}
+	
+	/**
+	 * Constructor making this BlockingQueue bounded
+	 * @param capacity The maximum number of elements.
+	 */
+	public CrawlQueue(int capacity) {
+		super();
+		this.capacity = capacity;
+		runnables = new LinkedList<Runnable>();
 	}
 
 	@Override
-	public synchronized int drainTo(Collection<? super Runnable> c, int maxRunnablelements) {
-		int counter = 0;
-		for (Runnable object : this) {
-			counter++;
-			if (counter < maxRunnablelements) {
-				c.add(object);
-			} else {
-				break;
-			}
+	public Runnable element() {
+		this.lock.lock();
+		try {
+			if (count.get() <= 0)
+				throw new NoSuchElementException();
+			else 
+				return runnables.get(count.get()-1);
+		} finally {
+			this.lock.unlock();
 		}
-		for (Object object : c) {
-			this.remove(object);
+	}
+
+	@Override
+	public Runnable peek() {
+		this.lock.lock();
+		try {
+			if (count.get() <= 0)
+				return null;
+			else
+				return runnables.get(count.get()-1);
+		} finally {
+			lock.unlock();
 		}
-		return counter;
-	}
-
-	@Override
-	public boolean offer(Runnable e) {
-		return this.add(e);
-	}
-
-	@Override
-	public boolean offer(Runnable e, long timeout, TimeUnit unit) {
-		return this.add(e);
-	}
-
-	@Override
-	public Runnable poll(long timeout, TimeUnit unit) {
-		return remove();
-	}
-
-	@Override
-	public void put(Runnable e) {
-		this.add(e);
-
-	}
-
-	@Override
-	public int remainingCapacity() {
-		return Integer.MAX_VALUE;
-	}
-
-	@Override
-	public Runnable take() {
-		return remove();
-	}
-
-	@Override
-	@GuardedBy("this")
-	public synchronized Runnable element() {
-		return this.get(this.size() - 1);
 	}
 
 	@Override
 	public Runnable poll() {
-		return remove();
+		this.lock.lock();
+		try {
+			if (count.get() <= 0)
+				return null;
+			else 
+				return removeAt(count.get()-1);
+		} finally {
+			this.lock.unlock();
+		}
 	}
 
 	@Override
-	@GuardedBy("this")
-	public synchronized Runnable remove() {
-		if (this.size() <= 0) {
-			return null;
+	public Runnable remove() {		
+		this.lock.lock();
+		try {
+			if (count.get() <= 0)
+				throw new NoSuchElementException();
+			else
+				return removeAt(count.get()-1);
+		} finally {
+			this.lock.unlock();
 		}
-		return this.remove(this.size() - 1);
+	}
+	
+	private Runnable removeAt(int index) {
+		Runnable result = runnables.remove(index);
+		count.decrementAndGet();
+		isNotFull.signal();
+		return result;
 	}
 
+	@Override
+	public boolean addAll(Collection<? extends Runnable> c) {
+		this.lock.lock();
+		try {			
+			if (runnables.addAll(c)) {
+				count.set(runnables.size());
+				isNotEmpty.signal();
+				return true;
+			}
+			else
+				return false;
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public void clear() {
+		this.lock.lock();
+		try {
+			runnables.clear();
+			count.set(0);
+			isNotFull.signal();
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		this.lock.lock();
+		try {
+			return runnables.containsAll(c);
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public boolean isEmpty() {		
+		this.lock.lock();
+		try {
+			return count.get()<=0 ? true : false;
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public Iterator<Runnable> iterator() {
+		this.lock.lock();
+		try {
+			return runnables.iterator();
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		this.lock.lock();
+		try {
+			if (runnables.removeAll(c)) {
+				count.set(runnables.size());
+				isNotFull.signal();
+				return true;
+			}
+			else
+				return false;
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		this.lock.lock();
+		try {
+			if (runnables.retainAll(c)) {
+				count.set(runnables.size());
+				isNotFull.signal();
+				return true;
+			}
+			else
+				return false;
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public int size() {
+		return count.get();
+	}
+
+	@Override
+	public Object[] toArray() {
+		this.lock.lock();
+		try {
+			return runnables.toArray();
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		this.lock.lock();
+		try {
+			return runnables.toArray(a);
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public boolean add(Runnable e) {
+		this.lock.lock();
+		try {
+			if (count.get() < capacity)
+				return addOneItem(e);
+			else
+				throw new IllegalStateException();
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	private boolean addOneItem(Runnable e) {
+		if (runnables.add(e)) {
+			count.incrementAndGet();
+			isNotEmpty.signal();
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	@Override
+	public boolean contains(Object o) {
+		this.lock.lock();
+		try {
+			return runnables.contains(o);
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public int drainTo(Collection<? super Runnable> c) {
+		this.lock.lock();
+		try {
+			c.addAll(runnables);
+			runnables.clear();
+			int cnt =count.get();
+			count.set(0);
+			isNotFull.signal();
+			return cnt;
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public int drainTo(Collection<? super Runnable> c, int maxElements) {
+		this.lock.lock();
+		try {
+			int cnt;			
+			if (maxElements > capacity)
+				cnt = capacity;
+			else if (maxElements > count.get())
+				cnt = count.get();
+			else 
+				cnt = maxElements;
+			
+			for (int i=0; i<cnt; i++) {
+				c.add(runnables.remove(count.get()-1));
+				count.decrementAndGet();				
+			}
+			isNotFull.signal();
+			return cnt;			
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public boolean offer(Runnable e) {
+		this.lock.lock();
+		try {
+			if (count.get() < capacity)
+				return addOneItem(e);
+			else
+				return false;
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public boolean offer(Runnable e, long timeout, TimeUnit unit)
+			throws InterruptedException {
+		this.lock.lock();
+		try {
+			if (count.get() < capacity) {				
+				return addOneItem(e);
+			}
+			else {
+				if (timeout <= 0)
+					return false;
+				try {
+					if (isNotFull.awaitNanos(unit.toNanos(timeout)) <=0 ) {
+						isNotFull.signal();
+						return false;
+					}
+					else 
+						return addOneItem(e);
+				} catch (InterruptedException ie) {
+					isNotFull.signal();
+					throw ie;
+				}
+			}
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public Runnable poll(long timeout, TimeUnit unit)
+			throws InterruptedException {
+		this.lock.lock();
+		try {
+			if (count.get() > 0) 				
+				return removeAt(count.get()-1);
+			else {
+				if (timeout <= 0)
+					return null;
+				try {
+					if (isNotEmpty.awaitNanos(unit.toNanos(timeout))<= 0) {
+						isNotEmpty.signal();
+						return null;
+					}
+					else
+						return removeAt(count.get()-1);	
+				}  catch (InterruptedException ie) {
+					isNotEmpty.signal();
+					throw ie;
+				}
+			} 
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public void put(Runnable e) throws InterruptedException {
+		this.lock.lock();
+		try {
+			if (count.get() < capacity) {
+				addOneItem(e);
+			}
+			else {
+				try {
+					while (!(count.get() < capacity))
+						isNotFull.await();
+					addOneItem(e);
+				} catch (InterruptedException ie) {
+					isNotFull.signal();
+					throw ie;
+				}
+			}
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public int remainingCapacity() {
+		this.lock.lock();
+		try {
+			return capacity - count.get();
+		} finally {
+			this.lock.unlock();
+		}		
+	}
+
+	@Override
+	public boolean remove(Object o) {		
+		this.lock.lock();
+		try {
+			if (count.get() == 0) 
+				return false;
+			else {
+				Boolean result = runnables.remove(o);
+				count.decrementAndGet();
+				isNotFull.signal();
+				return result;
+			}
+		} finally {
+			this.lock.unlock();
+		}
+	}
+
+	@Override
+	public Runnable take() throws InterruptedException {
+		this.lock.lock();
+		try {
+			if (count.get() > 0)
+				return removeAt(count.get()-1);				
+			else {
+				try {
+					while (!(count.get() > 0))
+						isNotEmpty.await();
+					return removeAt(count.get()-1);					
+				}  catch (InterruptedException ie) {	
+					isNotEmpty.signal();
+					throw ie;
+				}
+			} 
+		} finally {
+			this.lock.unlock();
+		}
+	}
 }
--- core/src/test/java/com/crawljax/core/CrawlerExecutorTest.java
+++ core/src/test/java/com/crawljax/core/CrawlerExecutorTest.java
@@ -10,7 +10,12 @@ import com.crawljax.browser.EmbeddedBrowser.BrowserType;
 import com.crawljax.core.configuration.BrowserConfiguration;
 
 /**
- * Test the CrawlerExecutor ThreadPoolExecutor. Basically it test only the correct naming.
+ * Original: Test the CrawlerExecutor ThreadPoolExecutor. Basically it test only the correct naming.</br></br>
+ * 
+ * Major Overhaul on March 24, 2013 by Jae-Hwan Jung <jaehwan.jeff.jung@gmail.com>: </br>
+ * Testing for correct naming does not work properly as scheduling of threads is done by the host OS.
+ * As a result, the order of statements may not be the same as the order of actual execution due to the loads
+ * of cores at time. 
  */
 public class CrawlerExecutorTest {
 
@@ -24,29 +29,100 @@ public class CrawlerExecutorTest {
 	 */
 	@Test
 	public void testCorrectNamesMultiThread() throws InterruptedException, ConfigurationException {
+		
 		CrawlerExecutor executor =
 		        new CrawlerExecutor(new BrowserConfiguration(BrowserType.firefox, 2));
-		TestThread t1 = new TestThread("Thread 1 Crawler 1", "");
-		TestThread t2 = new TestThread("Thread 2 Crawler 2 (Automatic)", "Automatic");
 		
-		executor.execute(t1);
+		/**
+		 * Since the default number of threads in the pool is 10, the first 10 runnables will be
+		 * assigned to the threads. Then the remaining threads will be inserted into the queue.
+		 * Then when the first 10 threads are done working, the queue will be popped in FILO order.
+		 */
+		TestThread t1 = new TestThread("Thread 1 Crawler 1", "");
+		TestThread t2 = new TestThread("Thread 2 Crawler 2", "");
+		TestThread t3 = new TestThread("Thread 3 Crawler 3", "");
+		TestThread t4 = new TestThread("Thread 4 Crawler 4", "");
+		TestThread t5 = new TestThread("Thread 5 Crawler 5", "");
+		TestThread t6 = new TestThread("Thread 6 Crawler 6", "");
+		TestThread t7 = new TestThread("Thread 7 Crawler 7", "");
+		TestThread t8 = new TestThread("Thread 8 Crawler 8", "");
+		TestThread t9 = new TestThread("Thread 9 Crawler 9", "");
+		TestThread t10 = new TestThread("Thread 10 Crawler 10", "");
+		/**
+		 * As mentioned above, these are the runnables in the queue and will be popped in FILO order.
+		 * Therefore the strings passed are different from above.
+		 */		
+		TestThread t11 = new TestThread("Thread 6 Crawler 16", "");
+		TestThread t12 = new TestThread("Thread 5 Crawler 15", "");
+		TestThread t13 = new TestThread("Thread 4 Crawler 14", "");
+		TestThread t14 = new TestThread("Thread 3 Crawler 13", "");
+		TestThread t15 = new TestThread("Thread 2 Crawler 12", "");
+		TestThread t16 = new TestThread("Thread 1 Crawler 11", "");
+		
+		
+		
 		/**
 		 * Slight delay between ThreadPoolExecutor.execute() is needed for this test to function correctly.
-		 * Depending on how the OS assigns JAVA threads to cores (or OS threads), 
-		 * the speed of execution of the methods could differ from the actual order of calling them. 
-		 * As a result, sometimes this test fails when executor.execute(t2) is actually executed
+		 * Depending on how the OS assigns JAVA threads to cores (or OS threads) and the load of the cores
+		 * at the moment, the speed of execution of the methods could differ from the actual order of 
+		 * calling them. As a result, sometimes this test fails when executor.execute(t2) is actually executed
 		 * before executor.execute(t1).
-		 * There are only two ways to fix this. First is to put a small delay (as Thread.sleep(1)) between
+		 * There are only two ways to fix this. First is to put a small delay (as Thread.sleep(SLEEPTIME)) between
 		 * method calling. Or assign only one core to JAVA SE process on Windows Task Manager (or equivalent
 		 * features of other OS).
 		 */
-		Thread.sleep(1);
+		final int SLEEPTIME = 10;
+		executor.execute(t1);
+		Thread.sleep(SLEEPTIME);		
 		executor.execute(t2);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t3);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t4);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t5);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t6);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t7);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t8);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t9);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t10);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t11);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t12);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t13);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t14);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t15);
+		Thread.sleep(SLEEPTIME);		
+		executor.execute(t16);
 
 		executor.waitForTermination();
 
 		assertThat(t1.appointedName, is(t1.expetedName));
 		assertThat(t2.appointedName, is(t2.expetedName));
+		assertThat(t3.appointedName, is(t3.expetedName));
+		assertThat(t4.appointedName, is(t4.expetedName));
+		assertThat(t5.appointedName, is(t5.expetedName));
+		assertThat(t6.appointedName, is(t6.expetedName));
+		assertThat(t7.appointedName, is(t7.expetedName));
+		assertThat(t8.appointedName, is(t8.expetedName));
+		assertThat(t9.appointedName, is(t9.expetedName));
+		assertThat(t10.appointedName, is(t10.expetedName));
+		assertThat(t11.appointedName, is(t11.expetedName));
+		assertThat(t12.appointedName, is(t12.expetedName));
+		assertThat(t13.appointedName, is(t13.expetedName));
+		assertThat(t14.appointedName, is(t14.expetedName));
+		assertThat(t15.appointedName, is(t15.expetedName));
+		assertThat(t16.appointedName, is(t16.expetedName));
+				
 	}
 
 	/**
@@ -67,8 +143,8 @@ public class CrawlerExecutorTest {
 		@Override
 		public void run() {
 			try {
-				Thread.sleep(500);
 				appointedName = Thread.currentThread().getName();
+				Thread.sleep(1000);				
 			} catch (InterruptedException | RuntimeException e) {
 				e.printStackTrace();
 			}
