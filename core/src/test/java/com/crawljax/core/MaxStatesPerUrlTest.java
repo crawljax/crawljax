780388ca8b41ba7aca751326cdde4079f2c944f2
David <david199110@hotmail.com>
2013-03-26 15:13:58 -0700

    added test for max states per url issue #156

---
+++ core/src/test/java/com/crawljax/core/MaxStatesPerUrlTest.java
@@ -0,0 +1,83 @@
+package com.crawljax.core;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.junit.Test;
+
+import com.crawljax.condition.NotXPathCondition;
+import com.crawljax.core.configuration.CrawljaxConfiguration;
+import com.crawljax.core.configuration.CrawljaxConfiguration.CrawljaxConfigurationBuilder;
+import com.crawljax.core.configuration.Form;
+import com.crawljax.core.configuration.InputSpecification;
+import com.crawljax.core.state.StateFlowGraph;
+import com.crawljax.core.state.StateVertex;
+
+/**
+ * Test class for limiting the max states per URL 
+ */
+public class MaxStatesPerUrlTest {
+
+	private static final int MAX_STATES_PER_URL = 3;
+	private static final long WAIT_TIME_AFTER_EVENT = 200;
+	private static final long WAIT_TIME_AFTER_RELOAD = 20;
+	private static final String URL = "http://spci.st.ewi.tudelft.nl/demo/crawljax/";
+	
+	private InputSpecification getInputSpecification() {
+		InputSpecification input = new InputSpecification();
+		Form contactForm = new Form();
+		contactForm.field("male").setValues(true, false);
+		contactForm.field("female").setValues(false, true);
+		contactForm.field("name").setValues("Bob", "Alice", "John");
+		contactForm.field("phone").setValues("1234567890", "1234888888", "");
+		contactForm.field("mobile").setValues("123", "3214321421");
+		contactForm.field("type").setValues("Student", "Teacher");
+		contactForm.field("active").setValues(true);
+		input.setValuesInForm(contactForm).beforeClickElement("button").withText("Save");
+		return input;
+	}
+
+	@Test
+	public void testMaxStatesPerUrl() throws CrawljaxException, ConfigurationException, InterruptedException {
+		CrawljaxConfigurationBuilder builder = CrawljaxConfiguration.builderFor(URL);
+		builder.crawlRules().insertRandomDataInInputForms(false);
+		
+		builder.setMaximumStatesPerUrl(MAX_STATES_PER_URL);
+		
+		builder.crawlRules().click("a");
+
+		// click these elements
+		builder.crawlRules().clickDefaultElements();
+		builder.crawlRules().click("div").withAttribute("class", "clickable");
+
+		// but don't click these
+		builder.crawlRules().dontClick("a").withAttribute("class", "ignore");
+		builder.crawlRules().dontClick("a").underXPath("//DIV[@id='footer']");
+
+		// Set timeouts
+		builder.crawlRules().waitAfterReloadUrl(WAIT_TIME_AFTER_RELOAD, TimeUnit.MILLISECONDS);
+		builder.crawlRules().waitAfterEvent(WAIT_TIME_AFTER_EVENT, TimeUnit.MILLISECONDS);
+
+		// Add a condition that this XPath doesn't exits
+		builder.crawlRules().addCrawlCondition("No spans with foo as class",
+		        new NotXPathCondition(
+		                "//*[@class='foo']"));
+
+		// Set some input for fields
+		builder.crawlRules().setInputSpec(getInputSpecification());
+
+		CrawljaxController crawljax = new CrawljaxController(builder.build());
+		
+		crawljax.run();
+		
+		StateFlowGraph graph = crawljax.getSession().getStateFlowGraph();
+
+		Object[] vertices = graph.getAllStates().toArray();
+		for(int i = 0; i < graph.getAllStates().size(); i++) {
+			assertTrue(graph.getOutgoingStates((StateVertex) vertices[i]).size() <= MAX_STATES_PER_URL);
+		}
+	}
+	
+}
