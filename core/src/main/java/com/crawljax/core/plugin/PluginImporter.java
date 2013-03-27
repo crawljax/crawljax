f5a45e56c6233d6d7900b0d4c44dd4032b6ab036
Kyle Lee <kylelee6@gmail.com>
2013-03-26 17:44:03 -0700

    Added a method to check classes of a jar file

--- core/src/main/java/com/crawljax/core/plugin/PluginImporter.java
+++ core/src/main/java/com/crawljax/core/plugin/PluginImporter.java
@@ -1,11 +1,14 @@
 package com.crawljax.core.plugin;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 import com.crawljax.*;
 
@@ -19,7 +22,7 @@ public class PluginImporter
     {
         _loader = loader;
     }
-    
+   /* 
     public static <T> List<T> getPluggedServices2(Class<T> clazz, ClassLoader loader)
     {
     	List<T> services = new ArrayList<T>();
@@ -38,7 +41,34 @@ public class PluginImporter
 		}
         return services;
     }
-
+*/
+    
+    public <T> List<T> getPluggedService (File jarfile)
+    {
+  
+    	List<T> services = new ArrayList<T>();
+    	try{
+    	 JarFile jarf = new JarFile(jarfile);
+    	 
+    	 Enumeration<JarEntry> jarEntries = jarf.entries();
+    	 
+    
+    	while (jarEntries.hasMoreElements())
+    	{
+    		JarEntry jarEntry = jarEntries.nextElement();
+    		services.add((T) jarEntry.getClass());
+    	}
+    	jarf.close();
+    	} catch (IOException e)
+        {
+        	e.printStackTrace();
+        	
+        }
+    		    	 
+    	  	
+    	 return services;
+    }
+    
     public <T> List<T> getPluggedServices(Class<T> clazz)
     {
         List<T> services = new ArrayList<T>();
