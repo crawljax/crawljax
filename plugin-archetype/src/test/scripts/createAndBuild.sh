eb19c740f295076b80e98ff24b00c5481dafdce5
Alex Nederlof <alex@nederlof.com>
2013-05-18 16:43:09 -0700

    Added a bit more documentation for the script

--- plugin-archetype/src/test/scripts/createAndBuild.sh
+++ plugin-archetype/src/test/scripts/createAndBuild.sh
@@ -1,8 +1,16 @@
+# Get the current dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Go to crawljax main folder
 cd $DIR/../../../../
+
+# Install the latest version locally
 mvn clean install -DskipTests=true
+
+#Use the target folder as a tmp folder
 cd $DIR/../../../target
 
+# Create the installed archtype with name 'testname'
 mvn archetype:generate -B \
   -DarchetypeGroupId=com.crawljax.plugins.archetypes \
   -DarchetypeArtifactId=crawljax-plugins-archetype \
@@ -11,7 +19,6 @@ mvn archetype:generate -B \
   -DartifactId=testname \
   -Dversion=1.0-SNAPSHOT
 
+# Open the generated project and compile
 cd testname
 mvn compile
-
-
