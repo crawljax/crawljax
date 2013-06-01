497ac93604d96acb183c4c7afc9d82549535d91d
Arie van Deursen <arie.vandeursen@tudelft.nl>
2013-06-01 17:24:38 +0200

    Turned ConditionTest into parameterized test.

--- core/src/test/java/com/crawljax/condition/ConditionTest.java
+++ core/src/test/java/com/crawljax/condition/ConditionTest.java
@@ -1,39 +1,51 @@
 package com.crawljax.condition;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * Since many of the AbstractConditions override equals and hashcode,
  * we provide some test cases for them.
  */
 
+@RunWith(Parameterized.class)
 public class ConditionTest {
     
     Condition self;
     Condition equivalent;
     Condition different;
     
-    @Before 
-    public void setup() {
-        self = new RegexCondition("rx0");
-        equivalent = new RegexCondition("rx0");
-        different = new RegexCondition("rx1");
+    public ConditionTest(Condition _self, Condition _equivalent, Condition _different) {
+        this.self = _self;
+        this.equivalent = _equivalent;
+        this.different = _different;
     }
     
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            { new RegexCondition("rx0"),
+              new RegexCondition("rx0"),
+              new RegexCondition("rx1")}
+        });
+    }
+       
     @Test
     public void testEquality() {
-        assertTrue(self.equals(equivalent));
-        assertTrue(equivalent.equals(self));
-        assertFalse(self.equals(different));
-        assertFalse(different.equals(self));
-        assertFalse(self.equals(null));
-        assertTrue(self.equals(self));
+        assertEquals(self, equivalent);
+        assertEquals(equivalent, self);
+        assertNotEquals(self, different);
+        assertNotEquals(different, self);
+        assertNotEquals(self, null);
+        assertEquals(self, self);
     }
     
     @Test
