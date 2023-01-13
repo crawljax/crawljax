cadd1eff289a163ca8f83ad1403c3d0b94eba34e
Rahul-IBM-Mac <rahulyk@gmail.com>
2023-01-13 10:03:11 -0700

    minor

--- core/src/test/java/com/crawljax/stateabstractions/hybrid/HybridStateVertexImplTest.java
+++ core/src/test/java/com/crawljax/stateabstractions/hybrid/HybridStateVertexImplTest.java
@@ -1,8 +1,16 @@
 package com.crawljax.stateabstractions.hybrid;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.List;
 
+import com.crawljax.browser.BrowserProvider;
+import com.crawljax.browser.EmbeddedBrowser;
+import com.crawljax.core.state.Eventable;
+import com.crawljax.core.state.Identification;
+import com.crawljax.fragmentation.FragmentationPlugin;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
 
 import com.crawljax.core.configuration.CrawljaxConfiguration;
@@ -15,15 +23,23 @@ import com.crawljax.vips_selenium.VipsRectangle;
 import com.crawljax.vips_selenium.VipsSelenium;
 
 import junit.framework.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import javax.imageio.metadata.IIOMetadataNode;
 
 public class HybridStateVertexImplTest {
+	@Rule
+	public final BrowserProvider provider = new BrowserProvider();
+	private HybridStateVertexFactory factory;
 
-	public static StateVertex getState(int id, String url, FragmentManager manager) {
-		VipsSelenium vips = new VipsSelenium(url);
+	public StateVertex getState(int id, String url, WebDriver driver, FragmentManager manager) {
+		VipsSelenium vips = new VipsSelenium(url, driver);
 		// VipsSeleniumParser parser = new VipsSeleniumParser(vips);
 		List<VipsRectangle> rectangles = vips.startSegmentation();
 		String domString = DomUtils.getDocumentToString(vips.dom);
-		HybridStateVertexFactory factory = new HybridStateVertexFactory(0, CrawljaxConfiguration.builderFor(url),
+		factory = new HybridStateVertexFactory(0, CrawljaxConfiguration.builderFor(url),
 				false);
 		StateVertex state = factory.newStateVertex(id, url, "test" + id, domString, domString, null);
 		// StateVertexImpl state = new StateVertexImpl(0,url, "test", domString,
@@ -41,16 +57,26 @@ public class HybridStateVertexImplTest {
 		return state;
 	}
 
+	public StateVertex getStateNew(int id, String url, EmbeddedBrowser browser, FragmentManager manager){
+		factory = new HybridStateVertexFactory(0, CrawljaxConfiguration.builderFor(url),
+				false);
+		StateVertex state = factory.newStateVertex(0, browser.getCurrentUrl(), "state" + id,
+				browser.getStrippedDom(), browser.getStrippedDomWithoutIframeContent(), browser);
+		FragmentationPlugin.fragmentState(state, manager, browser, new File("testOutput"), true);
+		return state;
+	}
+
 	@Ignore
 	@Test
 	public void testDynamicFragments() {
 		String url1 = "http://localhost:9966/petclinic/owners/2.html";
 		String url2 = "http://localhost:9966/petclinic/owners/10.html";
 		FragmentManager fragmentManager = new FragmentManager(null);
+		BrowserProvider provider = new BrowserProvider();
+		RemoteWebDriver driver  = provider.newBrowser();
+		StateVertex state1 = getState(0, url1, driver, fragmentManager);
 
-		StateVertex state1 = getState(0, url1, fragmentManager);
-
-		StateVertex state2 = getState(1, url2, fragmentManager);
+		StateVertex state2 = getState(1, url2, driver, fragmentManager);
 
 		StateComparision comp1 = fragmentManager.areND2(state1, state2);
 
@@ -59,4 +85,21 @@ public class HybridStateVertexImplTest {
 		Assert.assertEquals(StateComparision.NEARDUPLICATE2, comp);
 	}
 
+	@Test
+	public void testMDHFrag() throws InterruptedException {
+		System.setProperty("test.browser", "CHROME");
+		String url= "https://localhost:9448/mdh-tss/";
+		EmbeddedBrowser browser = provider.newEmbeddedBrowser();
+		WebDriver driver = browser.getWebDriver();
+		driver.navigate().to(url);
+		driver.findElement(By.tagName("body")).sendKeys("thisisunsafe");
+		UtilsSelenium.sendKeys(driver, driver.findElements(By.tagName("body")).get(0), "thisisunsafe");
+
+		UtilsSelenium.click(driver, driver.findElement(By.xpath("/html/body/div/div/div/app-root/gdpr-page/ibm-modal/ibm-overlay/section/div/ibm-modal-footer/footer/button[1]")));
+
+		Thread.sleep(5000);
+//		BufferedImage viewPort = browser.getScreenShotAsBufferedImage(500);
+		getStateNew(0, url, browser, new FragmentManager(null));
+		browser.close();
+	}
 }
