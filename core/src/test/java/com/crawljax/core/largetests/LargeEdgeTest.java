b0c8479ba0ed83cf8ee3436292b927345fa3e3ef
amesbah <amesbah@gmail.com>
2023-02-23 15:00:35 -0800

    add Edge driver support

---
+++ core/src/test/java/com/crawljax/core/largetests/LargeEdgeTest.java
@@ -0,0 +1,26 @@
+package com.crawljax.core.largetests;
+
+import com.crawljax.browser.EmbeddedBrowser.BrowserType;
+import com.crawljax.core.configuration.BrowserConfiguration;
+import com.crawljax.test.BrowserTest;
+import org.junit.experimental.categories.Category;
+
+@Category(BrowserTest.class)
+public class LargeEdgeTest extends LargeTestBase {
+
+  @Override
+  BrowserConfiguration getBrowserConfiguration() {
+    return new BrowserConfiguration(BrowserType.EDGE_HEADLESS);
+  }
+
+  @Override
+  long getTimeOutAfterReloadUrl() {
+    return 200;
+  }
+
+  @Override
+  long getTimeOutAfterEvent() {
+    return 200;
+  }
+
+}
