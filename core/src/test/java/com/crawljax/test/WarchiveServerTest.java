ceb00d7eeccef9b66f4933e6d9fab70ef4eca9ed
amesbah <amesbah@gmail.com>
2023-03-02 12:04:34 -0800

    make test more robust

--- core/src/test/java/com/crawljax/test/WarchiveServerTest.java
+++ core/src/test/java/com/crawljax/test/WarchiveServerTest.java
@@ -9,16 +9,23 @@ public class WarchiveServerTest {
 
     @Test
     public void testPort() {
-        server = new WarchiveServer("src/test/resources/warchives/petclinic", 8080);
+        int port = 8088;
+        server = new WarchiveServer("src/test/resources/warchives/petclinic", port);
         new Thread(server).start();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+
+        int timeout = 0;
+        while (!server.isStarted() && timeout < 10000) {
+            try {
+                Thread.sleep(100);
+                timeout += 100;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+
         Assert.assertTrue(
                 "server not started properly",
-                server.getSiteUrl().toString().equalsIgnoreCase("http://localhost:8080/"));
+                server.getSiteUrl().toString().equalsIgnoreCase("http://localhost:" + port + "/"));
         server.stop();
     }
 }
