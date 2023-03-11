f3e86eb310af1a4a388ee2104ee2f2dd8772b80e
DerekJLeggett <DerekJLeggett@gmail.com>
2023-03-11 07:33:16 -0700

    Fixed formatting.

--- examples/src/main/java/com/crawljax/examples/RemoteExample.java
+++ examples/src/main/java/com/crawljax/examples/RemoteExample.java
@@ -1,20 +1,17 @@
 package com.crawljax.examples;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.remote.Browser;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import com.crawljax.browser.EmbeddedBrowser.BrowserType;
 import com.crawljax.core.CrawljaxRunner;
 import com.crawljax.core.configuration.BrowserConfiguration;
-import com.crawljax.core.configuration.CrawljaxConfiguration;
 import com.crawljax.core.configuration.CrawlRules.FormFillMode;
+import com.crawljax.core.configuration.CrawljaxConfiguration;
 import com.crawljax.core.configuration.CrawljaxConfiguration.CrawljaxConfigurationBuilder;
 import com.crawljax.plugins.crawloverview.CrawlOverview;
 import com.crawljax.plugins.testcasegenerator.TestConfiguration;
-import com.crawljax.plugins.testcasegenerator.TestSuiteGenerator;
 import com.crawljax.plugins.testcasegenerator.TestConfiguration.StateEquivalenceAssertionMode;
+import com.crawljax.plugins.testcasegenerator.TestSuiteGenerator;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.Browser;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class RemoteExample {
 
@@ -27,7 +24,7 @@ public class RemoteExample {
     public static void main(String[] args) {
         String remoteUrl = "http://localhost:4444/wd/hub";
         int numberOfBrowsers = 1;
-        CrawljaxConfigurationBuilder builder = CrawljaxConfiguration.builderFor("http://demo.crawljax.com/");
+        CrawljaxConfigurationBuilder builder = CrawljaxConfiguration.builderFor("https://www.cnn.com/");
         builder.crawlRules().setFormFillMode(FormFillMode.NORMAL);
 
         builder.crawlRules().clickDefaultElements();
